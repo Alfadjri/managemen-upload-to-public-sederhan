111fbfed58979387380fbc8aafbf3754bcc3fe05
Alfadjri Dwi Fadhilah <alfadjri28@gmail.com>
2022-07-13 03:39:27 +0700

    Menghilangkan password

--- src/com/TugasGui/db_connect.java
+++ src/com/TugasGui/db_connect.java
@@ -4,5 +4,5 @@ public class db_connect {
     public  String SQL_Server = "jdbc:mariadb://localhost/";
     public  String databases = "jdbc:mariadb://localhost/db_bioskop";
     public  String user = "root";
-    public  String pwd = "push3321\"]1";
+    public  String pwd = "";
 }
